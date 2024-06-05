7d97ca27ad65396af320369dae630b25d2968f67
VRConservation <vance.russell@outlook.com>
2024-06-05 14:32:41 +0100

    neca chp edits

--- update.py
+++ update.py
@@ -5,12 +5,12 @@ import subprocess
 orig_dir = os.getcwd()
 
 # # run the ghp-import -n -p -f _build/html
-# subprocess.run(["ghp-import", "-n", "-p", "-f", "_build/html"], check=True)
+subprocess.run(["ghp-import", "-n", "-p", "-f", "_build/html"], check=True)
 
 # Run the command jb build --all . after the loop
 subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # # Push and sync the repository to GitHub make sure to change the 'Update repo' message
-# subprocess.run(["git", "add", "."], check=True)
-# subprocess.run(["git", "commit", "-m", "update.py testing"], check=True)
-# subprocess.run(["git", "push"], check=True)
+subprocess.run(["git", "add", "."], check=True)
+subprocess.run(["git", "commit", "-m", "neca chp edits"], check=True)
+subprocess.run(["git", "push"], check=True)
